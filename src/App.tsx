8394ee42218db4902d66add848458e6d82bf09b5
shasherazi <hassanrandomz@gmail.com>
2023-08-12 23:19:52 +0500

    Add Project component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import globalPlaylistScreenshot from './assets/screenshots/globalPlaylist.png'
 import { FaPython, FaReact } from 'react-icons/fa'
 import { SiFirebase, SiTypescript } from 'react-icons/si'
+import { Project } from './components/Project'
 
 const App = () => {
   return (
@@ -17,43 +18,18 @@ const App = () => {
       <div className='project'>
         <h2 className='font-bold text-2xl mb-2'>Projects</h2>
         <div className='project-list'>
-          <div className='global-playlist project-item'>
-            <img src={globalPlaylistScreenshot} alt='Global Playlist' />
-            <div className='project-item-description'>
-              <h2 className='font-bold text-lg'>Global Playlist</h2>
-              <div className='tech-used flex text-lg items-center gap-1 mb-2'>
-                <span className='font-bold'>Tech used: </span>
-                <FaReact />
-                <SiTypescript />
-                <FaPython />
-                <SiFirebase />
-              </div>
-              <p className='description leading-5 tracking-wide mb-1'>
-                Global Playlist is a website where anyone can add any song to a playlist.
-                This means you can discover new music worldwide or just rickroll your friends.
-                The project is open to anyone, so feel free to add your favorite songs and see
-                what others are listening to.
-              </p>
-              <div className='project-item-links flex gap-2'>
-                <a
-                  href='https://global-playlist.vercel.app/'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='underline'
-                >
-                  Live
-                </a>
-                <a
-                  href='https://github.com/shasherazi/globalPlaylist'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='underline'
-                >
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
+          <Project
+            title='Global Playlist'
+            description='Global Playlist is a website where anyone can add any song to a playlist.
+            This means you can discover new music worldwide or just rickroll your friends.
+            The project is open to anyone, so feel free to add your favorite songs and
+            see what others are listening to.'
+            image={globalPlaylistScreenshot}
+            imageAlt='Global Playlist'
+            techUsed={[FaReact, SiTypescript, FaPython, SiFirebase]}
+            liveLink='https://global-playlist.vercel.app/'
+            githubLink='https://github.com/shasherazi/globalPlaylist'
+          />
         </div>
       </div>
     </div>
---
+++ src/components/Project.tsx
@@ -0,0 +1,59 @@
+import { IconType } from "react-icons";
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  techUsed: IconType[];
+  liveLink: string;
+  githubLink: string;
+}
+
+export const Project = ({
+  title,
+  description,
+  image,
+  imageAlt,
+  techUsed,
+  liveLink,
+  githubLink,
+}: ProjectProps) => {
+  return (
+    <div className='project-item'>
+      <img src={image} alt={imageAlt} />
+      <div className='project-item-description'>
+        <h2 className='font-bold text-lg'>{title}</h2>
+        <div className='tech-used flex text-lg items-center gap-1 mb-2'>
+          <span className='font-bold'>Tech used: </span>
+          {
+            techUsed && techUsed.map((Icon, index) => (
+              <Icon key={index} />
+            ))
+          }
+        </div>
+        <p className='description leading-5 tracking-wide mb-1'>
+          {description}
+        </p>
+        <div className='project-item-links flex gap-2'>
+          <a
+            href={liveLink}
+            target='_blank'
+            rel='noreferrer'
+            className='underline'
+          >
+            Live
+          </a>
+          <a
+            href={githubLink}
+            target='_blank'
+            rel='noreferrer'
+            className='underline'
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+};
