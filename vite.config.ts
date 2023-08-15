885e9887490d8c05dac4d725c5f05ca1c6a6d449
shasherazi <hassanrandomz@gmail.com>
2023-08-15 14:58:45 +0500

    Performance optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({}),
+  ],
   base: '/portfolio/',
 })
