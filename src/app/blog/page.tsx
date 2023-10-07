3727c8d1adb67b1bfc07029c0af2c3866ee8d032
shasherazi <hassanrandomz@gmail.com>
2023-10-08 01:14:42 +0500

    Added blog route

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="blog h-screen flex items-center justify-center px-5">
+      <h1 className="text-4xl font-sans text-center">Under Construction 🚧</h1>
+    </div>
+  );
+}
