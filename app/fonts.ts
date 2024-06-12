a3f76b07e3f81acf5e374d64eb0f214d8ea7162d
shasherazi <hassanrandomz@gmail.com>
2024-06-13 00:47:20 +0500

    first draft

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import { Abril_Fatface, Poppins } from "next/font/google";
+
+export const abrilFatface = Abril_Fatface({
+  subsets: ["latin"],
+  weight: "400",
+});
+export const poppins = Poppins({ subsets: ["latin"], weight: "400" });
