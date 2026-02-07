6fc9b5456111c29815ecd6268e034a5c5b5aaad7
shasherazi <hassanrandomz@gmail.com>
2026-02-08 04:13:25 +0500

    added a bunch of styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,29 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+        provider: fontProviders.local(),
+        fallbacks: ["monospace"],
+        weights: ["100 900"],
+        subsets: ["latin"],
+        options: {
+          variants: [
+            {
+              src: ["./src/assets/fonts/JetBrainsMono-VariableFont_wght.ttf"],
+            },
+          ],
+        },
+      },
+    ],
   },
 });
