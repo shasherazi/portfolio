6b34b4389ca00ff3ced11271e242245b38b5ce1a
shasherazi <hassanrandomz@gmail.com>
2026-05-13 00:43:09 +0500

    added seo ig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://shasherazi.com",
   vite: {
     plugins: [tailwindcss()],
   },
@@ -36,6 +38,5 @@ export default defineConfig({
     ],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
-
